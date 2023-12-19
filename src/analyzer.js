9513b28ffefdc69996e261c4d62c5f65915a899d
Joseline0609 <jkgarciaalfaro@gmail.com>
2023-12-19 13:59:59 -0600

    Se agrega la funcionalidad de contar caracteres y contar caracteres sin espacios

--- src/analyzer.js
+++ src/analyzer.js
@@ -3,7 +3,19 @@ const analyzer = {
   getWordCount: (text) => {
     const words=text.split(' ').filter(Boolean);
     return words.length;
-  }, 
+  },
+  getCharacterCount: (text) => {
+    return text.length;
+  },
+  getCharacterCountExcludingSpaces: (text) => {
+    return text.replace(/\s+/g, '').length;
+  },
+  getAverageWordLength: (text) => {
+    const wordsForAverage=text.split(' ').filter(Boolean);
+    
+    
+  }
+
 
 };
 
--- src/index.js
+++ src/index.js
@@ -4,19 +4,27 @@ import analyzer from './analyzer.js'
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 document.addEventListener('DOMContentLoaded', () => {
   const textarea = document.querySelector('textarea');
-  const resultadoLi = document.querySelector('li[data-testid="word-count"]');
+  const resultadoLi0 = document.querySelector('li[data-testid="word-count"]');
+  const resultadoLi1=document.querySelector('li[data-testid="character-count"]');
+  const resultadoLi2=document.querySelector('Li[data-testid="character-no-spaces-count"]')
+  const resultadoLi6=document.querySelector('Li[data-testid="word-length-average]')
 
   // Añadir escuchador de eventos al textarea, para identificar cuando se realizen cambios
   textarea.addEventListener('input', () => {
     // Obtener el texto del textarea
     const texto = textarea.value;
-
     // Usar analyzer para obtener el recuento de palabras
     const wordCount = analyzer.getWordCount(texto);
-
+    const characterCount =analyzer.getCharacterCount(texto);
+    const noSpaces=analyzer.getCharacterCountExcludingSpaces(texto);
+    const lengthAverage=analyzer.getAverageWordLength(texto);
     // Actualizar el contenido del elemento li con el recuento de palabras
-    resultadoLi.textContent = `Cantidad de palabras: ${wordCount}`;
+    resultadoLi0.textContent = `Cantidad de palabras: ${wordCount}`;
+    resultadoLi1.textContent=`Cantidad de Caracteres: ${characterCount}`;
+    resultadoLi2.textContent=`Caracteres sin espacios: ${noSpaces}`;
+    resultadoLi6.textContent=`Promedio del largo de palabras: ${lengthAverage}`;
   });
+
 });
 
 
