ea7fdbaf0d160a4c843dfa748fcdc68cb260294e
Joseline0609 <jkgarciaalfaro@gmail.com>
2023-12-20 14:33:14 -0600

    Se agrega funcionalidad a las metricas restantes

--- src/analyzer.js
+++ src/analyzer.js
@@ -10,12 +10,66 @@ const analyzer = {
   getCharacterCountExcludingSpaces: (text) => {
     return text.replace(/\s+/g, '').length;
   },
-  getAverageWordLength: (text) => {
-    const wordsForAverage=text.split(' ').filter(Boolean);
-    
-    
-  }
+  getNumberCount: (text) => {
+    // Usar una expresión regular para encontrar todos los números en el texto
+    const numeros = text.match(/\d+/g);
+    // Devolver la cantidad de números encontrados
+    // Si no hay números, match devolverá null, así que asegúrate de manejar eso
+    return numeros ? numeros.length : 0;
+  },
+  getNumberSum: (text) => {
+    let suma = 0;
+    let numeroActual = '';
 
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] >= '0' && text[i] <= '9') {
+        // Agregar el dígito actual al número actual
+        numeroActual += text[i];
+      } else if (numeroActual.length > 0) {
+        // Sumar el número actual a la suma total y resetear el número actual
+        suma += parseInt(numeroActual, 10);
+        numeroActual = '';
+      }
+    }
+
+    // Asegurarse de sumar el último número si el texto termina con un dígito
+    if (numeroActual.length > 0) {
+      suma += parseInt(numeroActual, 10);
+    }
+
+    return suma;
+
+    /*const numeros = text.match(/\d+/g);
+    const suma = numeros ? numeros.reduce((total, num) => total + parseInt(num, 10), 0) : 0;
+    return suma;*/
+
+    /*const palabras = text.split(/\s+/);
+    let contadorNumeros = 0;
+    palabras.forEach(palabra => {
+      // Comprobar si la palabra es un número usando una expresión regular
+      if(palabra.match(/^\d+$/)) {
+        contadorNumeros++;
+      }
+    });
+    return contadorNumeros;*/
+  },
+  getAverageWordLength: (text) => {
+    const palabras = text.split(' ').filter(Boolean);
+    let sumaLongitudes = 0;
+
+    palabras.forEach(palabra => {
+      sumaLongitudes += palabra.length;
+    });
+
+    return palabras.length > 0 ? sumaLongitudes / palabras.length : 0;
+
+    /*const palabras = text.split(' ').filter(Boolean);
+    let sumaLongitudes = 0;
+    for (let i = 0; i < palabras.length; i++) {
+      sumaLongitudes += palabras[i].length;
+    }
+    return palabras.length > 0 ? sumaLongitudes / palabras.length : 0;*/
+  },
 
 };
 
--- src/index.js
+++ src/index.js
@@ -6,27 +6,34 @@ document.addEventListener('DOMContentLoaded', () => {
   const textarea = document.querySelector('textarea');
   const resultadoLi0 = document.querySelector('li[data-testid="word-count"]');
   const resultadoLi1=document.querySelector('li[data-testid="character-count"]');
-  const resultadoLi2=document.querySelector('Li[data-testid="character-no-spaces-count"]')
-  const resultadoLi6=document.querySelector('Li[data-testid="word-length-average]')
+  const resultadoLi2=document.querySelector('Li[data-testid="character-no-spaces-count"]');
+  const resultadoLi3=document.querySelector('Li[data-testid="number-count"]');
+  const resultadoLi4=document.querySelector('Li[data-testid="number-sum"]');
+  const resultadoLi5=document.querySelector('Li[data-testid="word-length-average"]');
 
   // Añadir escuchador de eventos al textarea, para identificar cuando se realizen cambios
   textarea.addEventListener('input', () => {
     // Obtener el texto del textarea
     const texto = textarea.value;
-    // Usar analyzer para obtener el recuento de palabras
+    // Usar analyzer para obtener el recuento de palabras y demas metricas
     const wordCount = analyzer.getWordCount(texto);
     const characterCount =analyzer.getCharacterCount(texto);
     const noSpaces=analyzer.getCharacterCountExcludingSpaces(texto);
+    const number=analyzer.getNumberCount(texto);
+    const numberSum=analyzer.getNumberSum(texto);
     const lengthAverage=analyzer.getAverageWordLength(texto);
-    // Actualizar el contenido del elemento li con el recuento de palabras
+    // Actualizar el contenido del elemento li con el recuento de palabras y demas metricas usando text.content
     resultadoLi0.textContent = `Cantidad de palabras: ${wordCount}`;
     resultadoLi1.textContent=`Cantidad de Caracteres: ${characterCount}`;
     resultadoLi2.textContent=`Caracteres sin espacios: ${noSpaces}`;
-    resultadoLi6.textContent=`Promedio del largo de palabras: ${lengthAverage}`;
+    resultadoLi3.textContent=`Numeros: ${number}`;
+    resultadoLi4.textContent=`Suma de numeros: ${numberSum}`;
+    resultadoLi5.textContent=`Promedio Longitud de Palabras: ${lengthAverage}`;
   });
 
 });
 
+//Falta separar la funcion y anexarla al doc. analyzer.js, asi como agregar otra funcionalidad para borrar tambien las metricas 
 
 const borrarContenido =document.querySelector('textarea');  
 const resetButton = document.getElementById('resetButton');
