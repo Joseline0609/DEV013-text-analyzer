b0c17f9f8dd16ea5b7e4a0b7b28734b307e572ac
Joseline0609 <jkgarciaalfaro@gmail.com>
2024-01-02 14:20:40 -0600

    Updates

---
+++ analyzer.js
@@ -0,0 +1,58 @@
+
+const analyzer = {
+  getWordCount: (text) => {
+    const words=text.split(' ').filter(Boolean);
+    return words.length;
+  },
+  getCharacterCount: (text) => {
+    return text.length;
+  },
+  getCharacterCountExcludingSpaces: (text) => {
+    return text.replace(/[\^\s*,.;@!"#$%&/()=?¡!¿'\\]/gi,"").length;
+    
+  },
+  getNumberCount: (text) => {
+    // Usar una expresión regular para encontrar todos los números en el texto
+    const numeros = text.match(/\d+([/.]\d+)?\b/g);
+    // Devolver la cantidad de números encontrados
+    return numeros ? numeros.length : 0;
+  },
+  getNumberSum: (text) => {
+    const expresionMatch = /\d+([/.]\d+)?\b/g;
+    const coincidencias = text.match(expresionMatch);
+    let suma = 0;
+
+    if (coincidencias) {
+      for (let i = 0; i < coincidencias.length; i++) {
+        suma += parseFloat(coincidencias[i]);
+      }
+    }
+
+    return suma;
+  },
+  getAverageWordLength: (text) => {
+    const palabras = text.split(' ').filter(Boolean);
+    let sumaLongitudes = 0;
+
+    palabras.forEach(palabra => {
+      sumaLongitudes += palabra.length;
+    });
+
+    const resultado =palabras.length > 0 ? sumaLongitudes / palabras.length : 0;
+    return Number(resultado.toFixed(2));
+
+    /*const palabras = text.split(' ').filter(Boolean);
+    let sumaLongitudes = 0;
+    for (let i = 0; i < palabras.length; i++) {
+      sumaLongitudes += palabras[i].length;
+    }
+    return palabras.length > 0 ? sumaLongitudes / palabras.length : 0;*/
+  },
+
+};
+
+/*function clear(contenido) {
+  contenido.value='';
+}*/
+
+export default analyzer;  
---
+++ index.js
@@ -0,0 +1,61 @@
+import analyzer from './analyzer.js'
+
+
+//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+document.addEventListener('DOMContentLoaded', () => {
+  const textarea = document.querySelector('textarea');
+  const resultadoLi0 = document.querySelector('li[data-testid="word-count"]');
+  const resultadoLi1=document.querySelector('li[data-testid="character-count"]');
+  const  resultadoLi2=document.querySelector('Li[data-testid="character-no-spaces-count"]');
+  const  resultadoLi3=document.querySelector('Li[data-testid="number-count"]');
+  const  resultadoLi4=document.querySelector('Li[data-testid="number-sum"]');
+  const  resultadoLi5=document.querySelector('Li[data-testid="word-length-average"]');
+
+  // Añadir escuchador de eventos al textarea, para identificar cuando se realizen cambios
+  textarea.addEventListener('input', () => {
+    // Obtener el texto del textarea
+    const texto = textarea.value;
+    // Llamar a analyzer para obtener el recuento de palabras y demas metricas
+    const wordCount=analyzer.getWordCount(texto);
+    const characterCount =analyzer.getCharacterCount(texto);
+    const noSpaces=analyzer.getCharacterCountExcludingSpaces(texto);
+    const number=analyzer.getNumberCount(texto);
+    const numberSum=analyzer.getNumberSum(texto);
+    const lengthAverage=analyzer.getAverageWordLength(texto);
+    // Actualizar el contenido del elemento li con el recuento de palabras y demas metricas usando text.content
+    resultadoLi0.textContent = `Cantidad de palabras: ${wordCount}`;
+    resultadoLi1.textContent=`Cantidad de Caracteres: ${characterCount}`;
+    resultadoLi2.textContent=`Caracteres sin espacios: ${noSpaces}`;
+    resultadoLi3.textContent=`Numeros: ${number}`;
+    resultadoLi4.textContent=`Suma de numeros: ${numberSum}`;
+    resultadoLi5.textContent=`Promedio Longitud de Palabras: ${lengthAverage}`;
+
+  });
+});
+
+//Falta separar la funcion y anexarla al doc. analyzer.js, asi como agregar otra funcionalidad para borrar tambien las metricas 
+
+const borrarContenido =document.querySelector('textarea');  
+const resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', function() {
+  borrarContenido.value = '';
+}
+);
+
+
+
+
+
+
+
+
+
+
+
+
+/*wordCount.value= '';
+      resultadoLi1.value= 0;
+      resultadoLi2.value='';
+      resultadoLi3.value='';
+      resultadoLi4.value='';
+      resultadoLi5.value='';*/
